ec496f3dc1cdf7fdba14dc98dcf86a16a445177b
Scott Bowler <scott.bowler@learningfuze.com>
2019-04-05 11:06:33 -0700

    Simple functional component button complete

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import '../assets/css/app.scss';
-import logo from '../assets/images/logo.svg';
+import { whatsYourName } from '../helpers';
+import ButtonFc from './button_fc'; // Button Functional Component
 
 const App = () => (
-    <div>
-        <div className="app">
-            <img src={logo} className="logo rotate"/>
-            <h1>Welcome To React</h1>
+    <div className="container">
+        <h1 className="center">React Examples</h1>
+
+        <div className="example-container center">
+            <h1>Example 1 - Button Functional Component</h1>
+            <ButtonFc text="Alert!" click={() => alert('The button was clicked')}/>
+            <ButtonFc backgroundColor="red" text="Are You Sure?" click={() => confirm('Are you sure you want to do this?')}/>
+            <ButtonFc backgroundColor="yellow" color="black" text="What's Your Name?" click={whatsYourName}/>
         </div>
     </div>
 );
