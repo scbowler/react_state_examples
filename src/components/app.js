adddadd0f6eefcce0b1d18a108b7c61569956600
Scott Bowler <scott.bowler@learningfuze.com>
2019-04-05 11:35:44 -0700

    Changing color button complete

--- src/components/app.js
+++ src/components/app.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '../assets/css/app.scss';
 import { whatsYourName } from '../helpers'; // Demo helper functions
 import BasicButton from './basic_button'; // Basic Button - Functional Component
+import ChangingButton from './changing_button'; // Changing color button - Class Component
 
 const App = () => (
     <div className="container">
@@ -12,6 +13,12 @@ const App = () => (
             <BasicButton text="Alert!" click={() => alert('The button was clicked')}/>
             <BasicButton backgroundColor="red" text="Are You Sure?" click={() => confirm('Are you sure you want to do this?')}/>
             <BasicButton backgroundColor="yellow" color="black" text="What's Your Name?" click={whatsYourName}/>
+        </div>
+
+        <div className="example-container center">
+            <h1>Example 2 - Changing Color Button - Class Component</h1>
+
+            <ChangingButton/>
         </div>
     </div>
 );
