e44891e17a2d7b9c6779039d69b9441c0913f422
Scott Bowler <scott.bowler@learningfuze.com>
2019-04-05 11:15:53 -0700

    Changed name of functional button component, ButtonFc -> BasicButton

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../assets/css/app.scss';
 import { whatsYourName } from '../helpers'; // Demo helper functions
-import ButtonFc from './button_fc'; // Button Functional Component
+import BasicButton from './basic_button'; // Basic Button - Functional Component
 
 const App = () => (
     <div className="container">
@@ -9,9 +9,9 @@ const App = () => (
 
         <div className="example-container center">
             <h1>Example 1 - Button Functional Component</h1>
-            <ButtonFc text="Alert!" click={() => alert('The button was clicked')}/>
-            <ButtonFc backgroundColor="red" text="Are You Sure?" click={() => confirm('Are you sure you want to do this?')}/>
-            <ButtonFc backgroundColor="yellow" color="black" text="What's Your Name?" click={whatsYourName}/>
+            <BasicButton text="Alert!" click={() => alert('The button was clicked')}/>
+            <BasicButton backgroundColor="red" text="Are You Sure?" click={() => confirm('Are you sure you want to do this?')}/>
+            <BasicButton backgroundColor="yellow" color="black" text="What's Your Name?" click={whatsYourName}/>
         </div>
     </div>
 );
